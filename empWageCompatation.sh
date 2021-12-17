6d2b2b805e8a0bea090632eb0f332913d0874db5
Mayank Singh <smayank748@gmail.com>
2021-12-17 09:33:02 +0530

    [Mayank] created feature to check attandance

--- empWageCompatation.sh
+++ empWageCompatation.sh
@@ -1,1 +1,10 @@
+#! /bin/bash/ -x
 echo "Welcome to emp wage computation"
+isPresent='1'
+checkAttendance=$((RANDOM%2))
+if [ $checkAttendance == $isPresent ]
+	then
+		echo "Employee is present"
+	else
+		echo "Employee is absent"
+fi
