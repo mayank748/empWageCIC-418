671a514679366eb98e814c089e8d1edbc7b2915f
Mayank Singh <smayank748@gmail.com>
2021-12-18 09:53:13 +0530

    [Mayank]implemented the check for working hour

--- empWageCompatation.sh
+++ empWageCompatation.sh
@@ -4,11 +4,12 @@ isPresent='1'
 isPartTime='2'
 WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
-PARTTIME_WORKING_HOUR=4
+PARTTIME_WORKING_HOUR=8
 WORKED_DAYS_FOR_MONTH=20
+TOTAL_WORKING_HOUr_POSSIBLE=100
 workedDay=1
 totalSalary=0
-#recivingWag=0
+previousTotalWorkingHour=0
 
 function wageCalculation(){
 	local workingHourPerDay=$1
@@ -18,17 +19,19 @@ function wageCalculation(){
 	totalSalaryworkedDaysForMonth=$(( $dayWage  + $totalSalaryworkedDaysForMonth ))
 	echo $totalSalaryworkedDaysForMonth
 }
-while [ $workedDay -le $WORKED_DAYS_FOR_MONTH ]
+while [ $workedDay -le $WORKED_DAYS_FOR_MONTH ] && [ $previousTotalWorkingHour -le $TOTAL_WORKING_HOUr_POSSIBLE ]
 do
 checkAttendance=$((RANDOM%3))
 	case $checkAttendance in 
 		$isPresent)
 			echo "Employee is present"
 			totalSalary=`wageCalculation $FULL_DAY_HOUR $WAGE_PER_HOUR $totalSalary`
+			previousTotalWorkingHour=$(($previousTotalWorkingHour + $FULL_DAY_HOUR ))
 			echo $totalSalary;;
 		$isPartTime)
 			echo "Employee is doing parttime"
 			totalSalary=`wageCalculation $PARTTIME_WORKING_HOUR $WAGE_PER_HOUR $totalSalary`
+			previousTotalWorkingHour=$(($previousTotalWorkingHour + $PARTTIME_WORKING_HOUR ))
 			echo $totalSalary;;
 		*)
 			echo "Employee is absent"
